db1ffea86b6f5ee90998c94e8cd9029a6ba5e40b
Rexap17 <100739819+Rexap17@users.noreply.github.com>
2022-08-03 12:29:26 +0930

    update

---
+++ Computer.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+#include "Human.h"
+
+using namespace std;
+
+Human::Human(){};
+
+char Human::makeMove(){
+    return type;
+};
+
+Human::~Human(){};
---
+++ Computer.h
@@ -0,0 +1,20 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Human{
+
+    protected:
+
+        int rand;
+
+    public:
+        Human();
+        ~Human();
+};
+
+#endif
