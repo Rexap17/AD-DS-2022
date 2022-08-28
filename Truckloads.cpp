fba228abf2cc2313b2ceb2b4d947d9f043019f84
RubidoRex <a1798025@student.adelaide.edu.au>
2022-08-28 22:54:49 +0930

    added truckloads

---
+++ Truckloads.cpp
@@ -0,0 +1,23 @@
+#include "Truckloads.h"
+#include <iostream>
+
+Truckloads::Truckloads(){}
+
+int Truckloads::numTrucks(int numCrates, int loadSize){
+  int crate1,crate2;
+
+  if(numCrates <= loadSize){
+    return 1;
+  };
+
+  if(numCrates%2 == 0){
+    crate1 = numCrates/2;
+    crate2 = numCrates/2;
+  }else{
+    crate1 = numCrates/2;
+    crate2 = numCrates/2+1;
+  };
+
+  return numTrucks(crate1,loadSize) + numTrucks(crate2,loadSize);
+}
+
