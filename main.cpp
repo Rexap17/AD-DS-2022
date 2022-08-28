75faf7aa13b76bcb8c4082d48d7107264d61f3a2
RubidoRex <a1798025@student.adelaide.edu.au>
2022-08-29 03:49:10 +0930

    updated main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,51 @@
 #include "Reverse.h"
 #include "Truckloads.h"
+#include "EfficientTruckloads.h"
 
 #include <iostream>
+#include <string>
 using namespace std;
 
+std::string getWord(string str, int num)
+{
+    std::string word = "";
+    int count = 0;
+    for (auto x : str)
+    {
+        if (x == ' ')
+        {   
+            count += 1;
+            if (count == num){
+              return word;
+            };
+            word = "";
+        }
+        else {
+            word = word + x;
+        }
+    }
+    return word;
+}
+
 int main(){
-  // question 1
   Reverse R1;
-
-  int testNum = 54321;
-  cout << testNum << endl;
-  cout <<  R1.reverseDigit(testNum) << endl;
-
-  std::string testString = "World";
-  cout << testString << endl;
-  cout << R1.reverseString(testString) << endl;
-
-  //question 2
   Truckloads T1;
-  cout << T1.numTrucks(1024,5) << endl;
+  EfficientTruckloads E1;
+
+  std::string input;
+  std::getline(std::cin >> std::ws, input);
+
+  std::string result = "";
+  int testNum = stoi(getWord(input,1));
+  result = result + std::to_string(R1.reverseDigit(testNum)) + " ";
+
+  std::string testString = getWord(input,2);
+  result = result + R1.reverseString(testString) + " ";
+
+  result = result + std::to_string(T1.numTrucks(stoi(getWord(input,3)),stoi(getWord(input,4)))) + " ";
+  result = result + std::to_string(E1.numTrucks(stoi(getWord(input,3)),stoi(getWord(input,4)))) + " ";
+
+  cout << result << endl;
 
   return 0;
 }
